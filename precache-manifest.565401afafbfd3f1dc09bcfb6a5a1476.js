692dc75ace04b07b8de8283ded129a8014360c02
Polina Fedorova <ps.fedorova@yandex.ru>
2020-07-21 07:55:32 +0500

    Updates

---
+++ precache-manifest.565401afafbfd3f1dc09bcfb6a5a1476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9176677e9547df5d170877f9473bd531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "118e7e6ebbd7ac383e54",
+    "url": "/static/css/main.3b0b583b.chunk.css"
+  },
+  {
+    "revision": "63c52f2ee94c624e9a78",
+    "url": "/static/js/2.f31a9f3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f31a9f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118e7e6ebbd7ac383e54",
+    "url": "/static/js/main.934b59b7.chunk.js"
+  },
+  {
+    "revision": "694d08d347fabaa9d7aa",
+    "url": "/static/js/runtime-main.1db10096.js"
+  }
+]);
